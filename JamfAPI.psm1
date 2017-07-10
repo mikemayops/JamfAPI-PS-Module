f6daaadb7598a28a28af552ea6b98aa8b901dfbd
Michael G. Mayoral <mmayoral89@gmail.com>
2017-07-10 16:47:23 -0400

    Added functionality to get-JamfComputer

--- JamfAPI.psm1
+++ JamfAPI.psm1
@@ -56,16 +56,29 @@ function Get-JamfMobileDevice {
     elseif ($id){
 
         foreach($i in $Id){
+
             $query = "mobiledevices/id/$i"
             $Uri = "$JssAPIurl/JSSResource/$query"
-            $R = Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential
-            $MobileDevice = $R.mobile_device
-            $DeviceName = $MobileDevice.general | select -ExpandProperty device_name
-            $DeviceId = $MobileDevice.general | select -ExpandProperty id
-            Add-Member -InputObject $MobileDevice -MemberType NoteProperty -Name "device_name" -Value $DeviceName
-            Add-Member -InputObject $MobileDevice -MemberType NoteProperty -Name "id" -Value $DeviceId
-            $MobileDevice
-            continue
+
+            if ($OutFilePath) {
+
+                Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential -OutFile $OutFilePath
+                Write-Output "File saved at $OutFilePath"
+
+            }
+            else {
+                
+                $R = Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential
+                $MobileDevice = $R.mobile_device
+                $DeviceName = $MobileDevice.general | select -ExpandProperty device_name
+                $DeviceId = $MobileDevice.general | select -ExpandProperty id
+                Add-Member -InputObject $MobileDevice -MemberType NoteProperty -Name "device_name" -Value $DeviceName
+                Add-Member -InputObject $MobileDevice -MemberType NoteProperty -Name "id" -Value $DeviceId
+                $MobileDevice
+                continue
+
+            }
+
         }
         
     }
@@ -192,6 +205,203 @@ function Get-JamfMobileDevice {
 
 }
 
+
+function Get-JamfComputer {
+    [Cmdletbinding()]
+    param(
+        [Parameter(Mandatory=$true)]$JssAPIurl,
+        [Parameter(Mandatory=$true)]
+        [System.Management.Automation.CredentialAttribute()]
+        $Credential,
+        [AllowNull()][String[]]$Id,
+        # Find Mobile device by name
+        [AllowNull()][String]$Name,
+        # Find Mobile Device by Serial Number
+        [AllowNull()][string]$SerialNumber,
+        # Find Mobile Device by Wifi Mac Address
+        [AllowNull()][string]$MacAddress,
+        # Find Mobile Device by udid
+        [AllowNull()][string]$udid,
+        # Find mobile device that matches criteria. Is able to take Wildcards (*).
+        [AllowNull()][string]$Match,
+        # Specifies a path to one or more locations.
+        [Parameter(Mandatory=$false,
+                   ParameterSetName="ParameterSetName",
+                   HelpMessage="Path to save location.")]
+        [AllowNull()][string]$OutFilePath
+        )
+    if ($Credential -eq $null) {
+        $Credential = Get-Credential
+    }
+
+    if (($id -eq $null) -and ($Name -eq "") -and ($SerialNumber -eq "") -and ($MacAddress -eq "") -and ($udid -eq "") -and ($Match -eq "")) {
+        
+        $Uri = "$JssAPIurl/JSSResource/computers"
+        if ($OutFilePath) {
+            
+            Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential -OutFile $OutFilePath
+            Write-Output "File saved at $OutFilePath"
+        }
+        else {
+
+            $R = Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential
+            $Computers = $R.computers.computer
+            return $Computers
+        }
+        
+    }
+    elseif ($id){
+
+        foreach($i in $Id){
+
+            $query = "computers/id/$i"
+            $Uri = "$JssAPIurl/JSSResource/$query"
+
+            if ($OutFilePath) {
+
+                Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential -OutFile $OutFilePath
+                Write-Output "File saved at $OutFilePath"
+
+            }
+            else {
+                
+                $R = Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential
+                $Computer = $R.computers
+                $DeviceName = $Computer.general | select -ExpandProperty name
+                $DeviceId = $Computer.general | select -ExpandProperty id
+                Add-Member -InputObject $Computer -MemberType NoteProperty -Name "computer_name" -Value $DeviceName
+                Add-Member -InputObject $Computer -MemberType NoteProperty -Name "id" -Value $DeviceId
+                $Computer
+                continue
+
+            }
+
+        }
+        
+    }
+    elseif ($Name) {
+
+        $query = "computers/name/$name"
+        $Uri = "$JssAPIurl/JSSResource/$query"
+
+        if ($OutFilePath) {
+
+            Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential -OutFile $OutFilePath
+            Write-Output "File saved at $OutFilePath"
+
+        }
+        else {
+
+            $R = Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential
+            $Computer = $R.computer
+            $DeviceName = $Computer.general | select -ExpandProperty name
+            $DeviceId = $Computer.general | select -ExpandProperty id
+            Add-Member -InputObject $Computer -MemberType NoteProperty -Name "computer_name" -Value $DeviceName
+            Add-Member -InputObject $Computer -MemberType NoteProperty -Name "id" -Value $DeviceId
+            return $Computer
+
+        }
+
+    }
+    elseif ($SerialNumber) {
+
+        $query = "computers/serialnumber/$SerialNumber"
+        $Uri = "$JssAPIurl/JSSResource/$query"
+        
+        if ($OutFilePath) {
+
+            Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential -OutFile $OutFilePath
+            Write-Output "File saved at $OutFilePath"
+
+        }
+        else {
+
+            $R = Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential
+            $Computer = $R.computer
+            $DeviceName = $Computer.general | select -ExpandProperty name
+            $DeviceId = $Computer.general | select -ExpandProperty id
+            Add-Member -InputObject $Computer -MemberType NoteProperty -Name "computer_name" -Value $DeviceName
+            Add-Member -InputObject $Computer -MemberType NoteProperty -Name "id" -Value $DeviceId
+            return $Computer
+
+        }
+
+    }
+    elseif ($MacAddress) {
+
+        $query = "computers/macaddress/$MacAddress"
+        $Uri = "$JssAPIurl/JSSResource/$query"
+        
+        if ($OutFilePath) {
+
+            Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential -OutFile $OutFilePath
+            Write-Output "File saved at $OutFilePath"
+
+        }
+        else {
+
+            $R = Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential
+            $Computer = $R.computer
+            $DeviceName = $Computer.general | select -ExpandProperty name
+            $DeviceId = $Computer.general | select -ExpandProperty id
+            Add-Member -InputObject $Computer -MemberType NoteProperty -Name "computer_name" -Value $DeviceName
+            Add-Member -InputObject $Computer -MemberType NoteProperty -Name "id" -Value $DeviceId
+            return $Computer
+
+        }
+
+    }
+    elseif ($udid) {
+
+        $query = "computers/udid/$udid"
+        $Uri = "$JssAPIurl/JSSResource/$query"
+        
+        if ($OutFilePath) {
+
+            Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential -OutFile $OutFilePath
+            Write-Output "File saved at $OutFilePath"
+
+        }
+        else {
+
+            $R = Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential
+            $Computer = $R.computer
+            $DeviceName = $Computer.general | select -ExpandProperty name
+            $DeviceId = $Computer.general | select -ExpandProperty id
+            Add-Member -InputObject $Computer -MemberType NoteProperty -Name "computer_name" -Value $DeviceName
+            Add-Member -InputObject $Computer -MemberType NoteProperty -Name "id" -Value $DeviceId
+            return $Computer
+
+        }
+
+    }
+    elseif ($Match) {
+
+        $query = "computers/match/$Match"
+        $Uri = "$JssAPIurl/JSSResource/$query"
+        
+        if ($OutFilePath) {
+
+            Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential -OutFile $OutFilePath
+            Write-Output "File saved at $OutFilePath"
+
+        }
+        else {
+
+            $R = Invoke-RestMethod -Uri $Uri -Method Get -Credential $Credential
+            $Computer = $R.computers
+            # $DeviceName = $Computer.general | select -ExpandProperty name
+            # $DeviceId = $Computer.general | select -ExpandProperty id
+            # Add-Member -InputObject $Computer -MemberType NoteProperty -Name "computer_name" -Value $DeviceName
+            # Add-Member -InputObject $Computer -MemberType NoteProperty -Name "id" -Value $DeviceId
+            return $Computer
+
+        }
+
+    }
+
+}
+
 # Get-JamfComputer
 
 function Get-JamfDeviceCommand {
@@ -235,7 +445,6 @@ function Get-JamfDeviceCommand {
     }
 
 }
-
 
 function Send-JamfDeviceCommand {
     [Cmdletbinding()]
